99daa1aa6687cb06e2d828f861bfe1267ed5cc8e
Siddharth <sidraw122@gmail.com>
2022-09-26 22:32:18 +0530

    Made first migartions by adding commands to docker-compose

--- app/app/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import SimpleTestCase
-from app import calc
-
-class CalcTests(SimpleTestCase):
-    
-    def test_add_func(self):
-        res=calc.add(10,11)
-        self.assertEqual(res,21)
